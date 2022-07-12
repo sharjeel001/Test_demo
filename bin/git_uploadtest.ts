ec5ec73f9e17921c340492038306a4b1975e2417
M.Sharjeel <muhammadsharjeel819@gmail.com>
2022-07-12 23:38:20 +0500

    Updated table

--- bin/git_uploadtest.ts
+++ bin/git_uploadtest.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "@aws-cdk/core";
 import { GitUploadtestStack } from '../lib/git_uploadtest-stack';
 
 const app = new cdk.App();
--- lib/git_uploadtest-stack.ts
+++ lib/git_uploadtest-stack.ts
@@ -1,10 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from "@aws-cdk/core";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
-export class GitUploadtestStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class GitUploadtestStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
+    let dataBase = new dynamodb.Table(this,"Sharjeel_table",{
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey:{
+        name: "id",
+        type:dynamodb.AttributeType.STRING 
+      },
+    });
+
   }
 }
