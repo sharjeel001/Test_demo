e507828080cdc6c603696731fcaf730737e1ef84
M.Sharjeel <muhammadsharjeel819@gmail.com>
2022-07-12 23:18:52 +0500

    a

--- lib/git_uploadtest-stack.ts
+++ lib/git_uploadtest-stack.ts
@@ -5,12 +5,6 @@ import { Construct } from 'constructs';
 export class GitUploadtestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GitUploadtestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
